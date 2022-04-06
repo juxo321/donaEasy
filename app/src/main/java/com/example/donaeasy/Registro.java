146e0d3026f37de72236e9ed1ec5b849f4bbbc95
Adbel79 <adbel79@gmail.com>
2022-04-05 22:30:13 -0500

    Creacion de registro

--- app/src/main/java/com/example/donaeasy/Registro.java
+++ app/src/main/java/com/example/donaeasy/Registro.java
@@ -1,15 +1,28 @@
 package com.example.donaeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.EditText;
 import android.widget.SpinnerAdapter;
 
 import android.os.Bundle;
 
 public class Registro extends AppCompatActivity {
 
+
+    EditText txtApodo;
+    EditText txtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        txtApodo = findViewById(R.id.txtApodo);
+        txtPassword = findViewById(R.id.txtPassword);
+    }
+
+    public void AccionRegistrar(View view){
+
     }
 }
