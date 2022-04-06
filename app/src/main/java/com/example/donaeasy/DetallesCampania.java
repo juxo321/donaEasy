0e87dc878746db013c2ab5b899e1061a98a5667f
Adbel79 <adbel79@gmail.com>
2022-04-06 08:00:37 -0500

    Implementacion alert

--- app/src/main/java/com/example/donaeasy/DetallesCampania.java
+++ app/src/main/java/com/example/donaeasy/DetallesCampania.java
@@ -1,8 +1,10 @@
 package com.example.donaeasy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DetallesCampania extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class DetallesCampania extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_campania);
     }
+
+    public void AlertEliminarCampaña(View view){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Eliminar campaña");
+        builder.setMessage("¿Estás seguro de eliminar la campaña?");
+
+        builder.setPositiveButton("Si", null);
+        builder.setNegativeButton("No", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
