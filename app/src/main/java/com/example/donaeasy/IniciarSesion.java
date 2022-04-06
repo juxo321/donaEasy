dece356c9f9a159a0597c8c7e1f9d13038084fbe
juxo321 <juxo321@gmail.com>
2022-04-05 22:29:27 -0500

    Colores e interfaz de inicio de sesion completa

--- app/src/main/java/com/example/donaeasy/IniciarSesion.java
+++ app/src/main/java/com/example/donaeasy/IniciarSesion.java
@@ -2,14 +2,22 @@ package com.example.donaeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.view.View;
 
 public class IniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iniciarSesion);
+        setContentView(R.layout.activity_iniciarsesion);
 
+    }
+
+    public void acitivityRegistrarse(View view){
+        Intent intentRegistrarse =new Intent(IniciarSesion.this, Registro.class);
+        startActivity(intentRegistrarse);
     }
 }
