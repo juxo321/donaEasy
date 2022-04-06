b11b9a19b5b51af3dee0679741897726a0ce1f6e
juxo321 <juxo321@gmail.com>
2022-04-06 07:06:53 -0500

    Añadiendo firebase

--- app/src/main/java/com/example/donaeasy/IniciarSesion.java
+++ app/src/main/java/com/example/donaeasy/IniciarSesion.java
@@ -1,10 +1,14 @@
 package com.example.donaeasy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -19,4 +23,23 @@ public class IniciarSesion extends AppCompatActivity {
         Intent intentRegistrarse =new Intent(IniciarSesion.this, Registrarse.class);
         startActivity(intentRegistrarse);
     }
+
+    public void iniciarSesion(View view){
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/donaeasy/Registrarse.java
+++ app/src/main/java/com/example/donaeasy/Registrarse.java
@@ -1,28 +1,85 @@
 package com.example.donaeasy;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Registrarse extends AppCompatActivity {
 
 
-    EditText txtApodo;
-    EditText txtPassword;
-    
+    private EditText txtUsuario;
+    private EditText txtPassword;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private  RadioButton radiobtnPaciente;
+    private  RadioButton radiobtnDonador;
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
 
-        txtApodo = findViewById(R.id.txtApodo);
+        txtUsuario = findViewById(R.id.txtUsuario);
         txtPassword = findViewById(R.id.txtPassword);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGrupo);
+        radiobtnPaciente = findViewById(R.id.RadioPaciente);
+        radiobtnDonador = findViewById(R.id.RadioDonador);
+
+
+
     }
 
-    public void AccionRegistrar(View view){
+    public void Registrarse(View view){
+        String tipo = "";
 
+        if(radiobtnDonador.isChecked()){
+            tipo = radiobtnDonador.getText().toString();
+        }else if (radiobtnPaciente.isChecked()){
+            tipo = radiobtnPaciente.getText().toString();
+        }
+
+        Map<String, Object> usuario = new HashMap<>();
+        usuario.put("usuario", txtUsuario.getText().toString());
+        usuario.put("contrasena", txtPassword.getText().toString());
+        usuario.put("tipo", tipo);
+
+        System.out.println(txtUsuario.toString());
+        System.out.println(txtPassword.toString());
+        System.out.println(tipo.toString());
+
+
+
+        // Add a new document with a generated ID
+        if(db.collection("usuarios").add(usuario).isSuccessful()){
+            Toast.makeText(this, "Usuario agregado exitosamente", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Usuario agregado exitosamente", Toast.LENGTH_SHORT).show();
+        }
+
+        Intent intentIniciarSesion =new Intent(Registrarse.this, IniciarSesion.class);
+        startActivity(intentIniciarSesion);
     }
 }
