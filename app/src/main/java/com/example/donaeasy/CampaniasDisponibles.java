af2d7bf44e12ac8aae81bd2cebce323df10f76e6
Ariana Tejeda <marinestrellita@hotmail.com>
2022-04-21 19:08:59 -0500

    commit ariana

--- app/src/main/java/com/example/donaeasy/CampaniasDisponibles.java
+++ app/src/main/java/com/example/donaeasy/CampaniasDisponibles.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CampaniasDisponibles extends AppCompatActivity {
-
+//cCU01
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
