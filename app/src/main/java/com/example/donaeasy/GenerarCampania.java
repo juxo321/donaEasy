e02e790c49f98e56d4458970a265ad211323d5cc
juxo321 <juxo321@gmail.com>
2022-04-24 15:48:53 -0500

    Creando funcionalidad campania

--- app/src/main/java/com/example/donaeasy/GenerarCampania.java
+++ app/src/main/java/com/example/donaeasy/GenerarCampania.java
@@ -6,10 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GenerarCampania extends AppCompatActivity {
 
     Button btnCrearCampania ;
+    TextView txtNombre;
+    TextView txtTipoSangre;
+    TextView txtDonadoresRequeridos;
+    TextView txtUbicacion;
+    TextView txtDescripcion;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +26,23 @@ public class GenerarCampania extends AppCompatActivity {
 
         btnCrearCampania = findViewById(R.id.btnCrearCampania);
 
+        TextView txtNombre = findViewById(R.id.txtNombre);
+        TextView txtTipoSangre = findViewById(R.id.txtTipoSangre);
+        TextView txtDonadoresRequerid = findViewById(R.id.txtDonadoresRequeridos);
+        TextView txtUbicacion = findViewById(R.id.txtUbicacion);
+        TextView txtDescripcion = findViewById(R.id.txtDescripcion);
+
         Paciente paciente = (Paciente) getIntent().getExtras().getSerializable("paciente");
         if(paciente.getCampania() !=null){
             btnCrearCampania.setEnabled(false);
         }
+
+        
+
+
+
+
+
 
 
     }
